81dae73e751d03a898760b8c693b333bc181992b
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-06-03 21:45:47 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([letter for letter in word])
+    def match(self, available_anagram):
+        match_available_anagram=[]     
+
+        for word in available_anagram:
+            if sorted([letter for letter in word]) ==self.word:
+               match_available_anagram.append(word)
+        return match_available_anagram       
